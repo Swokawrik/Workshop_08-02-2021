800325fbfb56e8c9ba74456a6f8ee5893c113e12
Swokawrik <ludovic.hou@epitech.eu>
2022-02-11 10:46:03 +0400

    new

--- neoftech/my_neofetch.sh
+++ neoftech/my_neofetch.sh
@@ -2,9 +2,13 @@
 
 email_sign="@"
 
-whoami ; echo -n $email_sign ;hostname
+echo -n $(whoami) ; echo -n $email_sign ;hostname
 hostnamectl | grep "Operating System" | sed 's/Operating System/OS/'
 hostnamectl | grep "Kernel"
-echo "Uptime: " ; uptime
-echo "Shell: "
+echo -n "Uptime:" ; uptime
+echo -n "Shell: "
 $SHELL --version | head -n 1
+echo "CPU`cat /proc/cpuinfo`" | grep "model name" | uniq | sed 's/model name/CPU/'
+echo "Memory: `free -m | grep "Mem" | awk '{print $3}'`MiB / `free -m | grep "Mem" | awk '{print $2}'`MiB"
+# echo -n "/"
+# free -m | grep "Mem" | awk '{print $2}' ; echo "MiB"
